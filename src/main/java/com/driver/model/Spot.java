abb782e308b659d8deb3c5eaa8a073d27cb321fc
Aqeeb <aqebmohammad@gmail.com>
2023-01-28 21:02:25 +0530

    fix3

--- src/main/java/com/driver/model/Spot.java
+++ src/main/java/com/driver/model/Spot.java
@@ -1,6 +1,7 @@
 package com.driver.model;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 @Entity
 public class Spot {
@@ -16,7 +17,7 @@ public class Spot {
     private ParkingLot parkingLot;
 
     @OneToMany(mappedBy = "spot",cascade = CascadeType.ALL)
-    private List<Reservation> reservationList;
+    private List<Reservation> reservationList = new ArrayList<>();
 
     public Spot() {
     }
